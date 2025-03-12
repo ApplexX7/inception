6736e8e249f3dd743a0cdf0884ab5901dc1e2c70
Mohammed Hilali <mohilali@e1r11p2.1337.ma>
2025-03-12 16:12:49 +0000

    Complte maria-db

--- src/maria-db/mydb-config.sh
+++ src/maria-db/mydb-config.sh
@@ -1,13 +1,21 @@
 #!/bin/bash
+export DB_USER="mohilali"
+export DB_USER_PASSWORD="password123"
+export DB_NAME="wib_data"
+export DB_ROOT_PASSOWRD="password1234"
 
 service mariadb start
 
 sleep 5
 
-mariadb -u $DB_USER -p$DB_PASSWORD -h $DB_HOST -e "CREATE DATABASE IF NOT EXISTS $DB_NAME;"
+mariadb -e "CREATE DATABASE IF NOT EXISTS \`${DB_NAME}\`;"
 
-if [ $? -eq 0 ]; then
-	echo "Database '$DB_NAME' created successfuly."
-else
-	echo "Failed to create databse '$DB_NAME'"
+mariadb -e "CREATE USER IF NOT EXISTS \`${DB_USER}\`@'%' IDENTIFIED BY '${DB_PASSWORD}';"
 
+mariadb -e "GRANT ALL PRIVILEGES ON \`${DB_NAME}\` .* TO \`${DB_USER}\`@'%';"
+
+mariadb -e "FLUSH PRIVILEGES"
+
+mysqladmin -u root -p$DB_ROOT_PASSWORD shutdown
+
+mysqld_safe --port=3306 --bind-address=0.0.0.0 --datadir='/var/lib/mysql'
